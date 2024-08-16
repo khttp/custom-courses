d757b63c8fd626df2ed87d0d1a4a95dd255e5368
abdelrahman khattab <abdelrahmankhattab9999@gmail.com>
2024-08-16 23:36:46 +0300

    finish crud courses operation

--- udemy_lite/routes/course.py
+++ udemy_lite/routes/course.py
@@ -1,28 +1,60 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from ..models.course import Course, Category, Content
+from sqlmodel import Session, select
+from ..database import get_session
+import uuid
 
 course_router = APIRouter()
 
 
 @course_router.get("/courses")
-async def get_courses() -> list[dict]:
-    return []
+async def get_courses(session: Session = Depends(get_session)) -> list[Course]:
+    courses = session.exec(select(Course)).all()
+    return courses
 
 
 @course_router.get("/courses/{course_id}")
-async def get_course_by_id(course_id: int):
-    pass
+async def get_course_by_id(
+    course_id: uuid.UUID, session: Session = Depends(get_session)
+) -> Course:
+    course = session.get(Course, course_id)
+    return course
 
 
 @course_router.post("/courses")
-async def create_course(course_data):
-    pass
+async def create_course(
+    course: Course, session: Session = Depends(get_session)
+) -> Course:
+    db_course = Course.model_validate(course)
+    session.add(db_course)
+    session.commit()
+    session.refresh(db_course)
+    return db_course
 
 
 @course_router.put("courses/{course_id}")
-async def update_course(course_id: int):
-    pass
+async def update_course(
+    course_id: uuid.UUID, NewCourse: Course, session: Session = Depends(get_session)
+) -> Course:
+    db_course = session.get(Course, course_id)
+    if not db_course:
+        raise HTTPException(status_code=404, detail="page not found")
+    course = NewCourse.model_dump(exclude_unset=True)
+    for key, value in course.items:
+        setattr(db_course, key, value)
+    session.add(db_course)
+    session.commit()
+    session.refresh(db_course)
+    return db_course
 
 
 @course_router.delete("courses/{course_id}")
-async def delete_course(course_id: int):
-    pass
+async def delete_course(
+    course_id: uuid.UUID, session: Session = Depends(get_session)
+) -> dict:
+    db_course = Session.get(Course, course_id)
+    if not db_course:
+        raise HTTPException(status_code=404, detail="page not found")
+    session.delete(db_course)
+    session.commit()
+    return {"OK": True}
--- udemy_lite/routes/user.py
+++ udemy_lite/routes/user.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
+from ..models.user import User
 
 user_router = APIRouter()
 
 
 @user_router.post("/register")
-async def register():
+async def create_user(user: User) -> User:
     pass
 
 
 @user_router.post("/login")
-async def login():
+async def login(user: User) -> User:
     pass
